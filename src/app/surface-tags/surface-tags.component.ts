4cc53644c82f64c5b0cdc2073297b1b6ffc339c4
li-na-k <klass.lina@gmail.com>
2023-10-18 10:39:40 +0200

    added surface april tags for second screen

--- src/app/surface-tags/surface-tags.component.ts
+++ src/app/surface-tags/surface-tags.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-surface-tags',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./surface-tags.component.css']
 })
 export class SurfaceTagsComponent {
-
+  @Input() secondScreen : boolean = false;
 }
