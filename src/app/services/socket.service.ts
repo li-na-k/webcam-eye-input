980fef547a46e1ba97f1c922268cd48c7c38ee53
li-na-k <klass.lina@gmail.com>
2024-10-11 11:20:12 +0200

    adapt to optimized backend (sending only used data)

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -6,7 +6,6 @@ import { AppState } from '../state/app.state';
 import { changeScreen, changeXPos, changeYPos } from '../state/eyetracking/eyetracking.action';
 import { selectCurrentScreen } from '../state/eyetracking/eyetracking.selector';
 import { Screens } from '../enums/screens';
-import { TaskEvaluationService } from './task-evaluation.service';
 
 @Injectable({
   providedIn: 'root'
@@ -32,16 +31,11 @@ export class SocketService {
     console.log("start sending gaze data")
     this.socket.emit("startSendingGazeData");
     this.listenTo("gazeData")
-    .pipe(
-      throttleTime(1000 / 60)  // Process at 60 Hz (16.67 ms)
-    )
     .subscribe((data : any) => {
       //store current x and y pos
-      if(data.gaze_on_surfaces[0]?.on_surf){ //gaze is on this screen 
-        this.store.dispatch(changeXPos({newx: data.gaze_on_surfaces[0].norm_pos[0]}));
-        this.store.dispatch(changeYPos({newy: data.gaze_on_surfaces[0].norm_pos[1]}));
+        this.store.dispatch(changeXPos({newx: data.norm_pos[0]}));
+        this.store.dispatch(changeYPos({newy: data.norm_pos[1]}));
         this.store.dispatch(changeScreen({newScreen: data.name}));
-      }
     });
   }
 
